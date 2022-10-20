94e7a0e99492b41b3dd83ac8a87bce84946e1f66
Rita Sola <rita.sola@adevinta.com>
2022-10-20 15:35:04 +0200

    complete unit test of Place with all categories

--- src/test/java/com/eatingplaces/eatingplaces/model/PlaceTest.java
+++ src/test/java/com/eatingplaces/eatingplaces/model/PlaceTest.java
@@ -12,7 +12,15 @@ class PlaceTest {
     void PlaceHaveId(){
      Place place = new Place();
      assertThat(place,hasProperty("id"));
-
+     assertThat(place,hasProperty("name"));
+     assertThat(place,hasProperty("city"));
+     assertThat(place,hasProperty("category"));
+     assertThat(place,hasProperty("shortDescription"));
+     assertThat(place,hasProperty("longDescription"));
+     assertThat(place,hasProperty("district"));
+     assertThat(place,hasProperty("price"));
+     assertThat(place,hasProperty("doNotMiss"));
+     assertThat(place,hasProperty("highlight"));
     }
 
 
